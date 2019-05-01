0f45537fc7275f9eff0842e3ea6a9aa56c03dacb
Ishmael Perez <ishperez@bu.edu>
2019-04-30 23:56:27 -0400

    First commit

---
+++ index.js
@@ -0,0 +1,202 @@
+/**
+ * Author: Michael Hadley, mikewesthad.com
+ * Asset Credits:
+ *  - Tuxemon, https://github.com/Tuxemon/Tuxemon
+ */
+
+const config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  parent: "game-container",
+  pixelArt: true,
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 0 }
+    }
+  },
+  scene: {
+    preload: preload,
+    create: create,
+    update: update
+  }
+};
+
+const game = new Phaser.Game(config);
+let cursors;
+let player;
+let showDebug = false;
+
+function preload() {
+  this.load.image("tiles", "assets/map/roguelikeCity_transparent.png");
+  this.load.tilemapTiledJSON("map", "assets/map/tileset.json");
+
+  // An atlas is a way to pack multiple images together into one texture. I'm using it to load all
+  // the player animations (walking left, walking right, etc.) in one image. For more info see:
+  //  https://labs.phaser.io/view.html?src=src/animation/texture%20atlas%20animation.js
+  // If you don't use an atlas, you can do the same thing with a spritesheet, see:
+  //  https://labs.phaser.io/view.html?src=src/animation/single%20sprite%20sheet.js
+  this.load.atlas("atlas", "assets/sprites/ish.png", "assets/sprites/ish_atlas.json");
+}
+
+function create() {
+  const map = this.make.tilemap({ key: "map" });
+
+  // Parameters are the name you gave the tileset in Tiled and then the key of the tileset image in
+  // Phaser's cache (i.e. the name you used in preload)
+  const tileset = map.addTilesetImage("city", "tiles");
+
+  // Parameters: layer name (or index) from Tiled, tileset, x, y
+  const floorLayer = map.createStaticLayer("Floor", tileset, 0, 0);
+  const decorLayer = map.createStaticLayer("Decorations", tileset, 0, 0);
+  const solidLayer = map.createStaticLayer("Solid objects", tileset, 0, 0);
+  const belowLayer = map.createStaticLayer("Below player", tileset, 0, 0);
+  const aboveLayer = map.createStaticLayer("Above player", tileset, 0, 0);
+
+  solidLayer.setCollisionByProperty({ collides: true });
+
+  // By default, everything gets depth sorted on the screen in the order we created things. Here, we
+  // want the "Above Player" layer to sit on top of the player, so we explicitly give it a depth.
+  // Higher depths will sit on top of lower depth objects.
+  aboveLayer.setDepth(10);
+
+  // Object layers in Tiled let you embed extra info into a map - like a spawn point or custom
+  // collision shapes. In the tmx file, there's an object layer with a point named "Spawn Point"
+  const spawnPoint = map.findObject("Objects", obj => obj.name === "Spawn Point");
+
+  // Create a sprite with physics enabled via the physics system. The image used for the sprite has
+  // a bit of whitespace, so I'm using setSize & setOffset to control the size of the player's body.
+  player = this.physics.add
+    .sprite(0, 0, "atlas", "ish-front")
+    .setSize(16, 26);
+
+  // Watch the player and worldLayer for collisions, for the duration of the scene:
+  this.physics.add.collider(player, solidLayer);
+
+  // Create the player's walking animations from the texture atlas. These are stored in the global
+  // animation manager so any sprite can access them.
+  const anims = this.anims;
+  anims.create({
+    key: "ish-left-walk",
+    frames: anims.generateFrameNames("atlas", {
+      prefix: "ish-left-walk.",
+      start: 0,
+      end: 3,
+      zeroPad: 3
+    }),
+    frameRate: 10,
+    repeat: -1
+  });
+  anims.create({
+    key: "ish-right-walk",
+    frames: anims.generateFrameNames("atlas", {
+      prefix: "ish-right-walk.",
+      start: 0,
+      end: 3,
+      zeroPad: 3
+    }),
+    frameRate: 10,
+    repeat: -1
+  });
+  anims.create({
+    key: "ish-front-walk",
+    frames: anims.generateFrameNames("atlas", {
+      prefix: "ish-front-walk.",
+      start: 0,
+      end: 3,
+      zeroPad: 3
+    }),
+    frameRate: 10,
+    repeat: -1
+  });
+  anims.create({
+    key: "ish-back-walk",
+    frames: anims.generateFrameNames("atlas", {
+      prefix: "ish-back-walk.",
+      start: 0,
+      end: 3,
+      zeroPad: 3
+    }),
+    frameRate: 10,
+    repeat: -1
+  });
+
+  const camera = this.cameras.main;
+  camera.startFollow(player);
+  camera.setBounds(0, 0, 16 * 32, 16 * 16);
+
+  cursors = this.input.keyboard.createCursorKeys();
+
+  // Help text that has a "fixed" position on the screen
+  this.add
+    .text(16, 16, 'Arrow keys to move\nPress "D" to show hitboxes', {
+      font: "18px monospace",
+      fill: "#000000",
+      padding: { x: 20, y: 10 },
+      backgroundColor: "#ffffff"
+    })
+    .setScrollFactor(0)
+    .setDepth(30);
+
+  // Debug graphics
+  this.input.keyboard.once("keydown_D", event => {
+    // Turn on physics debugging to show player's hitbox
+    this.physics.world.createDebugGraphic();
+
+    // Create worldLayer collision graphic above the player, but below the help text
+    const graphics = this.add
+      .graphics()
+      .setAlpha(0.75)
+      .setDepth(20);
+    solidLayer.renderDebug(graphics, {
+      tileColor: null, // Color of non-colliding tiles
+      collidingTileColor: new Phaser.Display.Color(243, 134, 48, 255), // Color of colliding tiles
+      faceColor: new Phaser.Display.Color(40, 39, 37, 255) // Color of colliding face edges
+    });
+  });
+}
+
+function update(time, delta) {
+  const speed = 175;
+  const prevVelocity = player.body.velocity.clone();
+
+  // Stop any previous movement from the last frame
+  player.body.setVelocity(0);
+
+  // Horizontal movement
+  if (cursors.left.isDown) {
+    player.body.setVelocityX(-speed);
+  } else if (cursors.right.isDown) {
+    player.body.setVelocityX(speed);
+  }
+
+  // Vertical movement
+  if (cursors.up.isDown) {
+    player.body.setVelocityY(-speed);
+  } else if (cursors.down.isDown) {
+    player.body.setVelocityY(speed);
+  }
+
+  // Normalize and scale the velocity so that player can't move faster along a diagonal
+  player.body.velocity.normalize().scale(speed);
+
+  // Update the animation last and give left/right animations precedence over up/down animations
+  if (cursors.left.isDown) {
+    player.anims.play("ish-left-walk", true);
+  } else if (cursors.right.isDown) {
+    player.anims.play("ish-right-walk", true);
+  } else if (cursors.up.isDown) {
+    player.anims.play("ish-back-walk", true);
+  } else if (cursors.down.isDown) {
+    player.anims.play("ish-front-walk", true);
+  } else {
+    player.anims.stop();
+
+    // If we were moving, pick and idle frame to use
+    if (prevVelocity.x < 0) player.setTexture("atlas", "ish-left");
+    else if (prevVelocity.x > 0) player.setTexture("atlas", "ish-right");
+    else if (prevVelocity.y < 0) player.setTexture("atlas", "ish-back");
+    else if (prevVelocity.y > 0) player.setTexture("atlas", "ish-front");
+  }
+}
